e4a70dd96bc2cc98cbacd657aff6d81ffd24cbe0
mbarndouka <mmbarndouka@gmail.com>
2023-08-10 21:14:35 +0200

    testimonial part

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,24 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "John Doe",
+        descripton:"A really good job, all aspects of the projectwere followed step by step with good results"
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Harry Clinton",
+        descripton:"A really good job, all aspects of the projectwere followed step by step with good results"
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Sara Cell",
+        descripton:"A really good job, all aspects of the projectwere followed step by step with good results"
+    }
+]
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./testimonials.css";
+
+const Testimonials = () => {
+    return (
+        <sectionn className="Testimonials container section">
+            <h2 className="section__title">My Clients Say</h2>
+            <span className="section__subtitle">Testimonial</span>
+
+            <div className="testimonial__container"></div>
+        </sectionn>
+    );
+};
+
+export default Testimonials;
