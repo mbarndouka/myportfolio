ec48d772c98c93f1cf56af4effd09b7e8e309f04
mbarndouka <mmbarndouka@gmail.com>
2023-08-10 21:20:08 +0200

    testmomial suite

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Data } from "./Data";
+
 import "./testimonials.css";
 
 const Testimonials = () => {
@@ -7,8 +9,18 @@ const Testimonials = () => {
             <h2 className="section__title">My Clients Say</h2>
             <span className="section__subtitle">Testimonial</span>
 
-            <div className="testimonial__container"></div>
-
+            <div className="testimonial__container">
+                {Data.map(({ id, image, title, description }) => {
+                    return (
+                        <div className="testimonial__card" key={id}>
+                            <img src={image} alt=""
+                                className="testimomial__img" />
+                            <h3 className="testimonial__name">{title}</h3>
+                            <p className="testimonial__description">{ description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </sectionn>
     );
 };
